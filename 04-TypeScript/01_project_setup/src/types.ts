9d0eb7531f70fd288c213cd70497d69ecf8317f7
arbazXcode <jmi.arbaz1@gmail.com>
2026-01-12 16:35:24 +0530

    learnt about data types in ts

---
+++ 04-TypeScript/01_project_setup/src/types.ts
@@ -0,0 +1,11 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let book_name = "atomic habits";
+
+let tea_flavour: string = "ginger tea";
+
+let order: number = 10;
+
+let status: boolean = false;
